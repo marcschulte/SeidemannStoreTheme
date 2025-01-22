2d526a28fe8786f55564585d45612e304d858de8
Marc Schulte <marc.schulte@8mylez.com>
2025-01-22 21:07:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SeidemannStoreTheme
+
+Simple store theme.
+
+## Description
---
+++ src/SeidemannStoreTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace SeidemannStoreTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class SeidemannStoreTheme extends Plugin implements ThemeInterface
+{
+}
